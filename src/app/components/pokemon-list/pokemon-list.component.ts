2b4cd94f9262ca81832fcbc08c07431463e2edbe
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-10-16 10:14:28 +0200

    ajout de css

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.scss']
+  styleUrls: [
+    './pokemon-list.component.scss'
+  ],
 })
 export class PokemonListComponent {
   title = 'toto';
